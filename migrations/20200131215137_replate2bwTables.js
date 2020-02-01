7bb02606568552ae0e55146c17733c514f652c04
newbi462 <isthisemailfree@gmail.com>
2020-02-01 11:32:02 -0500

    tables

--- migrations/20200131215137_replate2bwTables.js
+++ migrations/20200131215137_replate2bwTables.js
@@ -1,8 +1,39 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable("users", (table) => {
+      table.increments();// id column, integer, primary key, auto-increment
+      table.string("username").index().notNullable();
+      table.string("password").index().notNullable();
+      table.string("phoneNumber").index().notNullable();
+      table.string("bizName").index();
+      table.string("bizAddress").index();
+      table.string("userType").index().notNullable();
+    })
+    .createTable("pickupRequest", (table) => {
+      table.increments();// id column, integer, primary key, auto-increment
+      table.string("typeOfFood").index().notNullable();
+      table.integer("qty").index().notNullable();
+      table.string("preferredPickupTime").index().notNullable();
+      table.integer("bizUserID")
+          .unsigned()
+          .notNullable()
+          .references("id").inTable("users")
+          .onDelete("RESTRICT")
+          .onUpdate("CASCADE");
+      table.boolean("VolClaimed").defaultTo(false);
+      table.integer("volUserID")
+          .unsigned()
+          .notNullable()
+          .references("id").inTable("users")
+          .onDelete("RESTRICT")
+          .onUpdate("CASCADE");
+      table.boolean("delivered").defaultTo(false);
+    });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists("pickupRequest")
+    .dropTableIfExists("users");
 };
