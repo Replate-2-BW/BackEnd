aea8f019fd08e0c9b8b953047f3796242bbbd370
newbi462 <isthisemailfree@gmail.com>
2020-02-01 20:37:22 -0500

    temp

--- knexfile.js
+++ knexfile.js
@@ -39,19 +39,22 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'sqlite3',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: "./data/replate2bw.db3",
+    },
+    useNullAsDefault: true, // ONLY needed for SQLite
+
+    migrations: {
+        directory: "./migrations",
+    },
+    seeds: {
+        directory: "./seeds",
     },
     pool: {
-      min: 2,
-      max: 10
+        afterCreate: (conn, done) => {
+            conn.run("PRAGMA foreign_keys = ON", done); // tur on foreign key enforcement
+        },
     },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
 
 };
